688fd14fd14d316d544211fa01f3e6f63d740ca2
Jim Heald <james.r.heald@gmail.com>
2017-08-30 15:33:14 -0700

    Let's see if it accepts this now

--- genOfficers.js
+++ genOfficers.js
@@ -12,7 +12,7 @@
 //}
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://devjimheald.com:8000/officers/", false);
+xhr.open("GET", "http://devjimheald.com:8080/cplug/officers/", false);
 xhr.send(null);
 var html = xhr.responseText;
 
